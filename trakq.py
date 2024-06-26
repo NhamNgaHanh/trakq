2a56b05ef1d296c56db9acf2d53c8e254551d86d
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-06-26 11:33:33 +0900

    Update trakq.py

--- trakq.py
+++ trakq.py
@@ -122,69 +122,59 @@ with tab1:
     st.write(num_d)
 with tab2:
     # Danh sách để lưu số hàng thỏa mãn điều kiện
-    #bd = st.number_input("Biên độ của cầu")
-    nd = int(st.number_input("Số ngày soi cầu"))
-    #nd = 3
+    nd1 = int(st.number_input("Số ngày soi cầu"))
     jd = int(st.number_input("Số ngày bạn muốn tính kết quả:"))
     bd = 1
-    #if bd == 0 :
-        #bd = 5
-        #st.write("Bên độ soi cầu là: ",bd)
     num_r = []
     numx = []
     numy = []
     num_d = []
     # Duyệt qua các hàng của DataFrame
-    #st.write("Ngày dừng lại để tính toán",df.iloc[kd, 0])
+    # st.write("Ngày dừng lại để tính toán",df.iloc[kd, 0])
     if st.button("Nhấn nút này để tính toán"):
         my_bar = st.progress(0)
-        for kd in range(1,jd):
-            my_bar.progress(((kd)/(jd-1)), "Đang tính toán, vui lòng đợi:")
-            if nd > 2:
-                for u in range(0, nd):
-                    x_u = str(int(df.iloc[u+kd, 1]))[-2:]
+        for kd in range(1, jd):
+            my_bar.progress(((kd) / (jd - 1)), "Đang tính toán, vui lòng đợi:")
+            if nd1 > 2:
+                for u in range(0, nd1):
+                    x_u = str(int(df.iloc[u + kd, 1]))[-2:]
                     y_u = df.iloc[u, 0]
                     numx.append(x_u)
                     numy.append(y_u)
-                    #if u > 0:
-                        #st.write(int(numx[u]) - int(numx[u-1]))
-            #st.write(str(numx),df.iloc[0, 0])
-            #st.write(kd)
-            for bd in range(1,16):
-                #st.subheader(f":red[Biên độ dao động của cầu = {bd}]")
-                #st.write(kd)
-                if nd > 2:
+                    # if u > 0:
+                    # st.write(int(numx[u]) - int(numx[u-1]))
+            # st.write(str(numx),df.iloc[0, 0])
+            # st.write(kd)
+            for bd in range(1, 16):
+                # st.subheader(f":red[Biên độ dao động của cầu = {bd}]")
+                # st.write(kd)
+                if nd1 > 2:
                     kml = kd + 1
-                    for i in range(kml,len(df)-20):
-                        l = int(str(int(df.iloc[i, 1]))[-2:])  # Lấy hai ký tự cuối cùng của giá trị và chuyển thành chuỗi
+                    for i in range(kml, len(df) - 20):
+                        l = int(
+                            str(int(df.iloc[i, 1]))[-2:])  # Lấy hai ký tự cuối cùng của giá trị và chuyển thành chuỗi
                         l1 = int(str(int(df.iloc[i + 1, 1]))[-2:])
                         x1 = l1 - l
                         if (int(numx[1]) - int(numx[0])) - bd <= x1 <= (int(numx[1]) - int(numx[0])) + bd:
                             match_found = True
-                            for p in range(2, nd):
-                                l_p = int(str(int(df.iloc[i+p, 1]))[-2:])
-                                l_f = int(str(int(df.iloc[i+p-1, 1]))[-2:])
+                            for p in range(2, nd1):
+                                l_p = int(str(int(df.iloc[i + p, 1]))[-2:])
+                                l_f = int(str(int(df.iloc[i + p - 1, 1]))[-2:])
                                 x_p = l_p - l_f
-                                if (int(numx[p]) - int(numx[p-1]))- bd <= x_p <= (int(numx[p]) - int(numx[p-1])) + bd:
-                                    if p == nd - 1:
-                                        lits=[]
-                                        for m in range(0,nd):
-                                            pl = i+m
+                                if (int(numx[p]) - int(numx[p - 1])) - bd <= x_p <= (
+                                        int(numx[p]) - int(numx[p - 1])) + bd:
+                                    if p == nd1 - 1:
+                                        lits = []
+                                        for m in range(0, nd1):
+                                            pl = i + m
                                             lits.append(df.iloc[pl, 1])
-                                        num_r.append(str(int(df.iloc[i-1, 1]))[-2:])
-                                        #st.write(df.iloc[i, 0])
-                                        #st.write(lits,)
-                                        #st.write(str(int(df.iloc[i-1, 1]))[-2:])
+                                        num_r.append(str(int(df.iloc[i - 1, 1]))[-2:])
                                 else:
                                     break
-                    #st.write("Các chỉ số hàng thỏa mãn điều kiện:")
-                    #st.write(str(num_r))
                     tl = len(num_r)
-                    #st.write(tl)
-                    #st.write(nd)
                     lon = 0
                     be = 0
-                    for nu in range(0,tl):
+                    for nu in range(0, tl):
                         kh = int(num_r[nu])
                         if kh > 50:
                             lon = lon + 1
@@ -193,14 +183,14 @@ with tab2:
                     ks = df.iloc[kd, 0]
                     st.session_state["ex"] = ks
                     write_to_excel(ks, kd, 0, excel_file)
-                    ks1 = int(str(int(df.iloc[kd -1, 1]))[-2:])
+                    ks1 = int(str(int(df.iloc[kd - 1, 1]))[-2:])
                     st.session_state["ex"] = ks1
                     write_to_excel(ks1, kd, 17, excel_file)
                     if tl != 0:
-                        #st.write("Số cầu thõa mãn là :",tl)
-                        gttt = int(str(int(df.iloc[kd -1, 1]))[-2:])
-                        if round((lon/tl)*100,2) > round((be/tl)*100,2):
-                            if round((lon/tl)*100,2) > 50 and gttt >= 50:
+                        # st.write("Số cầu thõa mãn là :",tl)
+                        gttt = int(str(int(df.iloc[kd - 1, 1]))[-2:])
+                        if round((lon / tl) * 100, 2) > round((be / tl) * 100, 2):
+                            if round((lon / tl) * 100, 2) > 50 and gttt >= 50:
                                 ks = "Thắng"
                                 st.session_state["ex"] = ks
                                 write_to_excel(ks, kd, bd, excel_file)
@@ -229,13 +219,13 @@ with tab2:
             numx = []
             numy = []
         data = pd.read_excel("./result.xlsx")
-    
+
         # Tạo một đối tượng io.BytesIO để ghi dữ liệu vào
         output = io.BytesIO()
-    
+
         # Ghi dữ liệu DataFrame vào đối tượng io.BytesIO
         data.to_excel(output, index=False)
-    
+
         # Tạo nút tải với dữ liệu đã ghi vào đối tượng io.BytesIO
         btn = st.download_button(
             label="Download Excel File",
